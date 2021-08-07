f9a83c68cc3e469d26db425d119d7cafbab64472
Elliot Olbright <elliotolbright@gmail.com>
2021-08-07 17:06:17 -0600

    user stroy 3 complete and tested

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -14,6 +14,20 @@ class Merchant::DiscountsController < ApplicationController
   end
 
   def create 
-    
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.new(discount_params)
+    if @discount.save
+      redirect_to merchant_discounts_path(params[:merchant_id])
+      flash[:notice] = "#{@discount.name} successfully Created."
+    else
+      redirect_to new_merchant_discount_path(params[:merchant_id])
+      flash[:alert] = "All fields are required."
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:name, :quantity, :discount, :merchant_id)
   end
 end
--- spec/features/merchants/discounts/new_spec.rb
+++ spec/features/merchants/discounts/new_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe 'Merchant Invoice New Page' do
     expect(current_path).to eq(new_merchant_discount_path(@merchant.id))
   end
 
-  
+  it 'can create a new discount' do 
+    fill_in('name', with: 'Flash Sale')
+    fill_in('discount', with: 0.15)
+    fill_in('quantity', with: 5)
+    click_on 'Create Discount'
+
+    expect(current_path).to eq(merchant_discounts_path(@merchant.id))
+    expect(page).to have_content('Flash Sale')
+  end 
 end
