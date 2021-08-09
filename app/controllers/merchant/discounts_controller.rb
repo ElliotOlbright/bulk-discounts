d070e5f11c267b6572a59fb6c1324dbd6914884c
Elliot Olbright <elliotolbright@gmail.com>
2021-08-09 12:50:34 -0600

    test covereage back to 100 for edit page

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,20 +1,19 @@
 class Merchant::DiscountsController < ApplicationController 
+
+  before_action :find_discount, only: [:show, :edit, :update, :destroy]
+  before_action :find_merchant, only: [:index, :show, :edit, :new, :create, :update]
+
   def index 
-    @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.discounts
   end 
 
   def show
-    @merchant = Merchant.find(params[:merchant_id]) 
-    @discount = Discount.find(params[:id])
   end
 
   def new 
-    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def create 
-    @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.new(discount_params)
     if @discount.save
       redirect_to merchant_discounts_path(params[:merchant_id])
@@ -26,31 +25,33 @@ class Merchant::DiscountsController < ApplicationController
   end
 
   def edit 
-    @merchant = Merchant.find(params[:merchant_id])
-    @discount = Discount.find(params[:id])
   end
 
   def update 
-    merchant = Merchant.find(params[:merchant_id])
-    discount = Discount.find(params[:id]) 
-    require "pry"; binding.pry
-    if discount.update(discount_model_params)
-      flash[:alert] = "Successfully Updated #{discount.name}"
-      redirect_to merchant_discount_path(merchant.id, discount.id)
+    if @discount.update(discount_model_params)
+      flash[:alert] = "Successfully Updated #{@discount.name}"
+      redirect_to merchant_discount_path(@merchant.id, @discount.id)
     else 
       flash[:alert] = "All fields are required."
-      redirect_to edit_merchant_discount_path(merchant.id, discount.id)
+      redirect_to edit_merchant_discount_path(@merchant.id, @discount.id)
     end
   end
 
   def destroy 
-    @discount =  Discount.find(params[:id]) 
     @discount.destroy
     redirect_to merchant_discounts_path(params[:merchant_id])
     flash[:notice] = "#{@discount.name} Successfully Deleted."
   end
 
   private
+
+  def find_discount
+    @discount = Discount.find(params[:id])
+  end 
+  
+  def find_merchant 
+    @merchant = Merchant.find(params[:merchant_id]) 
+  end 
 
   def discount_params
     params.permit(:name, :quantity, :discount, :merchant_id)
